2a4c8078eb71b892d2d348c77cdff61bcfbe4a90
tekofx <tekofxx@gmail.com>
2024-02-21 09:13:06 +0100

    First commit

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+interface Config {
+  BOT_TOKEN: string;
+}
+
+let config: Config;
+
+let missingEnvVars = [];
+
+if (!process.env.BOT_TOKEN) missingEnvVars.push("MYSQL_DATABASE");
+
+if (missingEnvVars.length > 0) {
+  console.error(`Missing environment variables: ${missingEnvVars.join(", ")}`);
+  process.exit(1);
+}
+
+config = {
+  BOT_TOKEN: process.env.BOT_TOKEN!,
+};
+export { config };
