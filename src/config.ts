e086a9c5c408cb2959acc9cb05770d7dab91fe64
tekofx <tekofxx@gmail.com>
2024-02-22 10:18:53 +0100

    Added MYSQL_ROOT_PASSWORD

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ interface Config {
   MYSQL_USER: string;
   MYSQL_PASSWORD: string;
   MYSQL_DATABASE: string;
+  MYSQL_ROOT_PASSWORD: string;
 }
 
 let config: Config;
@@ -15,6 +16,8 @@ if (!process.env.MYSQL_HOST) missingEnvVars.push("MYSQL_HOST");
 if (!process.env.MYSQL_USER) missingEnvVars.push("MYSQL_USER");
 if (!process.env.MYSQL_PASSWORD) missingEnvVars.push("MYSQL_PASSWORD");
 if (!process.env.MYSQL_DATABASE) missingEnvVars.push("MYSQL_DATABASE");
+if (!process.env.MYSQL_ROOT_PASSWORD)
+  missingEnvVars.push("MYSQL_ROOT_PASSWORD");
 
 if (missingEnvVars.length > 0) {
   console.error(`Missing environment variables: ${missingEnvVars.join(", ")}`);
@@ -27,5 +30,6 @@ config = {
   MYSQL_USER: process.env.MYSQL_USER!,
   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD!,
   MYSQL_DATABASE: process.env.MYSQL_DATABASE!,
+  MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD!,
 };
 export { config };
